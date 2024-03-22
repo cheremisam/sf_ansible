cae7f85332c7d5ebfd1bffad62feb02faebdd1d5
Artem Cheremys <cheremis.am@gmail.com>
2024-03-22 22:46:49 +1000

    First commit

---
+++ main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_version = "1.5.7"
+
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.112.0"
+    }
+  }
+
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "tf-state-bucket"
+    region     = "ru-central1-a"
+    key        = "lemp.tfstate"
+    access_key = "YCAJEgYjnAnOU5pVU_5Z4UCpq"
+    secret_key = "YCOkGPbQBsX5fW7YZdKLo4R8htdAIM8VwX4bTlcW"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+
+}
+
+# Документация к провайдеру тут https://registry.terraform.io/providers/yandex-cloud/yandex/latest/docs#configuration-reference
+# Настраиваем the Yandex.Cloud provider
+provider "yandex" {
+  service_account_key_file = file("sa.json")
+  cloud_id                 = "b1gplktasqvig8ni6vo5"
+  folder_id                = var.folder_id
+  zone                     = "ru-central1-a"
+}
+
+resource "yandex_vpc_network" "terraform-network" {
+  name      = "terraform-network"
+  folder_id = var.folder_id
+}
+
+resource "yandex_vpc_subnet" "subnet1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.terraform-network.id
+  folder_id      = var.folder_id
+  v4_cidr_blocks = ["192.168.0.0/24"]
+}
+
+module "compute_instance_1" {
+  source        = "./modules/compute_instance"
+  family_image  = "ubuntu-2204-lts"
+  vpc_subnet_id = yandex_vpc_subnet.subnet1.id
+  zone          = yandex_vpc_subnet.subnet1.zone
+}
---
+++ output.tf
@@ -0,0 +1,7 @@
+output "internal_ip_address_vm_1" {
+  value = module.compute_instance_1.internal_ip_address_vm
+}
+
+output "external_ip_address_vm_1" {
+  value = module.compute_instance_1.external_ip_address_vm
+}
